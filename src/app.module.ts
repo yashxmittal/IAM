48dea1c135d536765456e03a6e910efae4051b97
yashxmittal <mittal.yash3333@gmail.com>
2023-07-09 19:47:54 +0530

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from './database/redis/redis.module';
+import { LoggerModule } from './utils/logger/logger.module';
+import { AuthModule } from './api/auth/auth.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [RedisModule, LoggerModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
